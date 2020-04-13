fdfd70aee290dcc0d25f1402c82ac7695eea7394
Cobus <cobus@moduteq.com>
2020-04-13 16:23:34 +0200

    initial commit

---
+++ tabler.py
@@ -0,0 +1,32 @@
+import os
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import Table, MetaData
+
+class Tabler():
+    def __init__(self):
+        if not os.getenv("DATABASE_URL"):
+            raise RuntimeError("DATABASE_URL is not set")
+        self.engine = create_engine(os.getenv("DATABASE_URL"))
+        self.db = scoped_session(sessionmaker(bind=self.engine))
+
+
+    def create_tables(self):
+        # Set up database
+        query = 'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username TEXT UNIQUE, hash TEXT NOT NULL)'
+        self.engine.execute(query)
+        
+    def print_table_entries(self):
+        md = MetaData()
+        for entry in self.engine.table_names():
+            table = Table(entry, md, autoload=True, autoload_with=self.engine)
+            #resp = self.db.execute("SELECT * FROM users", {"table": entry}).fetchall()
+            #print(f"{entry} : {resp}")
+            print(entry)
+            for c in table.columns:
+                print(f"{c.name} - {c.type}")
+
+if __name__ == "__main__":
+    test = Tabler()
+    test.create_tables()
+    test.print_table_entries()
